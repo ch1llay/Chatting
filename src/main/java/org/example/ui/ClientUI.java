ca9706bf0cdf1707de54a06300dea557dcd1d630
i.chapaev <i.chapaev@smartway.today>
2024-04-27 23:05:22 +0300

    add ui

--- src/main/java/org/example/ui/ClientUI.java
+++ src/main/java/org/example/ui/ClientUI.java
@@ -1,6 +1,8 @@
 package org.example.ui;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,6 +14,7 @@ import javafx.stage.Stage;
 
 
 public class ClientUI extends Application {
+    String firstElem = "Выберите получателя";
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Create a grid pane to hold the components
@@ -26,20 +29,19 @@ public class ClientUI extends Application {
         grid.add(textField, 0, 1);
         grid.add(sendButton, 2, 1);
 
-
-        // Set an event handler for the button
-        sendButton.setOnAction((ActionEvent e) -> {
-            // Set the text of the text field to "Hello World"
-            textField.setText("Hello World");
-        });
-
         // Set the stage and scene
         primaryStage.setScene(new Scene(grid, 520, 250));
         primaryStage.show();
 
         ComboBox<String> combo = new ComboBox<>();
-        combo.getItems().addAll("Выберите получателя");
-        combo.getSelectionModel().selectFirst();
+        combo.getItems().addAll(firstElem, "123");
+        combo.getSelectionModel().clearAndSelect(0);
+        combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                System.out.println(t1);
+            };
+        });
         grid.add(combo, 0, 2);
 
         // Add event handler for button press
